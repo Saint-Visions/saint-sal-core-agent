188a665356629a8a5998bc802f4c637d36b3ced7
Saint-Visions <ryan@saintvisiongroup.com>
2025-04-05 12:09:36 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,10 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  root: '.',
   build: {
     outDir: 'dist',
     rollupOptions: {
-      input: './public/index.html'
+      input: 'index.html'
     }
   }
 })
